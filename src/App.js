782e228a449549954b86dc42f74b219034eb1b85
Melissa Lynch <melissalynch@Melissas-MacBook.local>
2020-01-10 13:39:48 -0500

    push App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import {Table, TableRow, TableCell} from './components';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      numRows: 0,
+      numCols: 1,
+      selectedColor: "#f0e3ff"
+    }
+  }
+  addRow = () => {
+    this.setState({numRows: this.state.numRows+1})
+    //console.log(this.state.numRows);
+
+  }
+  addCol = () => {
+    this.setState({numCols: this.state.numCols+1})
+
+  }
+  changeColor = (e) => {
+    this.setState({selectedColor:e.target.value});
+  }
+  render() {
+    return (
+      <div className="App">
+      <button className="add-row-button" onClick={this.addRow}>Add Row</button>
+      <button className="add-col-button" onClick={this.addCol}>Add Col</button>
+      <select onChange ={this.changeColor}>
+        <option value="#f0e3ff">purple1</option>
+        <option value="#d89cf6">purple2</option>
+        <option value="#3e206d">purple3</option>
+        <option value="limegreen">limegreen</option>
+        <option value="hotpink">hotpink</option>
+        <option value="PowderBlue">powderblue</option>
+      </select>
+      <Table numRows={this.state.numRows} numCols={this.state.numCols} 
+        selectedColor={this.state.selectedColor} />
+      </div>
+    );
+  }
 }
 
 export default App;
